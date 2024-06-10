dcbea5a3cdbf655388d7c3d84681cd3a698a558a
Filipe Patricio <facpatricio@gmail.com>
2024-06-10 16:15:49 +0200

    Initial Commit

---
+++ SwiftfulMapApp/SwiftfulMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulMapAppApp.swift
+//  SwiftfulMapApp
+//
+//  Created by Filipe Patricio on 10/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
