0c0b6a9bf47654f4c13435d49b5d13e3eb5664cf
Filipe Patricio <facpatricio@gmail.com>
2024-06-10 17:13:47 +0200

    Add Map

--- SwiftfulMapApp/ViewModels/LocationsViewModel.swift
+++ SwiftfulMapApp/ViewModels/LocationsViewModel.swift
@@ -6,12 +6,34 @@
 //
 
 import Foundation
+import MapKit
+import SwiftUI
 
 class LocationsViewModel: ObservableObject {
+    // All loaded locations
     @Published var locations: [Location]
+
+    // Current location on the map
+    @Published var mapLocation: Location {
+        didSet {
+            updateMapRegion(location: mapLocation)
+        }
+    }
+
+    @Published var mapRegion: MKCoordinateRegion = .init()
+    let mapSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
 
     init() {
         let locations = LocationsDataService.locations
         self.locations = locations
+        self.mapLocation = locations.first!
+        updateMapRegion(location: mapLocation)
+    }
+
+    private func updateMapRegion(location: Location) {
+        withAnimation(.easeInOut) {
+            mapRegion = MKCoordinateRegion(center: location.coordinates,
+                                           span: mapSpan)
+        }
     }
 }
--- SwiftfulMapApp/Views/LocationsView.swift
+++ SwiftfulMapApp/Views/LocationsView.swift
@@ -5,16 +5,16 @@
 //  Created by Filipe Patricio on 10/06/2024.
 //
 
+import MapKit
 import SwiftUI
 
 struct LocationsView: View {
     @EnvironmentObject private var vm: LocationsViewModel
 
     var body: some View {
-        List {
-            ForEach(vm.locations) { location in
-                Text(location.name)
-            }
+        ZStack {
+            Map(coordinateRegion: $vm.mapRegion)
+                .ignoresSafeArea()
         }
     }
 }
