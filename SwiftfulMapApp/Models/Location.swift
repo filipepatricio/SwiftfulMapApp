8d4c5da9766b66928b18b05e5733ccbb08ad5689
Filipe Patricio <facpatricio@gmail.com>
2024-06-10 16:27:15 +0200

    Setup Project

---
+++ SwiftfulMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftfulMapApp
+//
+//  Created by Filipe Patricio on 10/06/2024.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
